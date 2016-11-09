e063e83546112dca513c24bd9a501f7f117061ad
Alain Hoang <alain.hoang@gsa.gov>
2016-11-09 16:42:14 -0600

    WIP for #74.
    * Add tests for installation check
    * Enable/disable tests for rulesets

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,14 +1,39 @@
 #!/usr/bin/env bats
 
+GIT_TESTREPO="gittest"
+REPO_PATH="${BATS_TMPDIR}/${GIT_TESTREPO}"
+
+setupGitRepo() {
+    git init "${REPO_PATH}"
+}
+
+cleanGitRepo() {
+    (cd "${REPO_PATH}" && git reset --hard)
+}
+
+addFileWithNoSecrets() {
+    local filename="${REPO_PATH}/plainfile.md"
+
+    touch "${filename}"
+    echo "Just a plain old file" >> "${filename}"
+    (cd "${REPO_PATH}" && git add "${filename}")
+    (cd ${REPO_PATH} && git commit -m 'test commit')
+}
+
+addFileWithSecrets() {
+    local secrets_file="${REPO_PATH}/secretsfile.md"
+
+    touch "${secrets_file}"
+    echo "SHHHH... Secrets in this file" >> "${secrets_file}"
+    echo "key: $(seq -s '' 0 1 24)" >> "${secrets_file}"
+    (cd "${REPO_PATH}" && git add "${secretsfile}")
+    (cd ${REPO_PATH} && git commit -m 'test commit')
+}
+
 setup() {
-    # Preserve enabled rulesets before each test
-    BATS_SEEKRETS_SAVED_RULES=$(git-seekret rules | grep '\[x\] '| awk ' { print $2 } ')
+    setupGitRepo
 }
 
 teardown() {
-    # Re-enable preserved rulesets post test
-    for seeketrule in $BATS_SEEKRETS_SAVED_RULES
-    do
-	git-seekret rule --enable "${seekretrule}"
-    done
+    cleanGitRepo
 }
