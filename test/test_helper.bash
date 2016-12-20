a3eca2fa07e15c84dbb6bd8f86ecac7420b2879f
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-12-20 17:03:13 -0500

    Make sure that both configs are overwritten;
    Regardless of precedent!

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -6,6 +6,7 @@ LOCAL_SEEKRETS="$(dirname $BATS_TEST_DIRNAME)/seekret-rules"
 setupGitRepo() {
     git init "${REPO_PATH}"
     git config --local gitseekret.rulespath "$LOCAL_SEEKRETS"
+    git config --global gitseekret.rulespath "$LOCAL_SEEKRETS"
     (cd "${REPO_PATH}" && git-seekret rules --enable-all)
 }
 
